428f28bd75c4c5c0adf094c2b63104c644feb071
David Kelly <davek032@outlook.com>
2019-11-19 15:05:38 +0000

    Core player functions
    Player now actually appears in the scene(Head & torso)
    Player has hands(Which can grab things)
    Player is correctly positioned on the wagon, with idle horses in front of them

---
+++ Assets/Scripts/WagonController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// General player facing systems
+/// Currently contains:
+/// >Horse animation controller
+/// </summary>
+
+public class WagonController : MonoBehaviour
+{
+    public GameObject LeftHorse;
+    public GameObject RightHorse;
+
+    Animator LeftHorseAnim;
+    Animator RightHorseAnim;
+
+    void Start()
+    {
+        LeftHorseAnim = LeftHorse.GetComponent<Animator>();
+        RightHorseAnim = RightHorse.GetComponent<Animator>();
+
+        LeftHorseAnim.SetFloat("Speed_f", 0);
+        RightHorseAnim.SetFloat("Speed_f", 0);
+    }
+
+}

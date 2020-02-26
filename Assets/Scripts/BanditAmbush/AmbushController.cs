65eb3e1fd2d6a4dbc81bd86d461f8745f0d8ef7e
David Kelly <davek032@outlook.com>
2020-02-26 19:15:45 +0000

    Most logic for ambush scene done
    Need to work out why the countdown isn't working as intended

---
+++ Assets/Scripts/BanditAmbush/AmbushController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AmbushController : MonoBehaviour
+{
+    public GameObject SafeRoute;
+    public GameObject safeExit;
+    public GameObject DangerousRoute;
+    public GameObject dangerousExit;
+
+
+    GameObject lookingAtRoute;
+    NavMeshAgent agent;
+
+    private void Awake()
+    {
+        agent = GetComponentInParent<NavMeshAgent>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        lookingAtRoute = other.gameObject;
+
+        if (lookingAtRoute == SafeRoute)
+        {
+            MakeYourDecision();
+            agent.SetDestination(safeExit.transform.position);
+        }
+        if (lookingAtRoute == DangerousRoute)
+        {
+            StartCoroutine(MakeYourDecision());
+            agent.SetDestination(dangerousExit.transform.position);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        lookingAtRoute = null;
+    }
+
+    IEnumerator MakeYourDecision() 
+    {
+        yield return new WaitForSeconds(30f);
+    }
+
+}

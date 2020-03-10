285d1419d8ba39a734d5858a25213e7bbe9a356f
David Kelly <davek032@outlook.com>
2020-03-10 19:46:35 +0000

    Main menu done

---
+++ Assets/Scripts/MainMenu/MenuController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuController : MonoBehaviour
+{
+    public GameObject startButton;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject == startButton)
+        {
+            StartCoroutine(MenuCountdown());
+        }
+    }
+
+    IEnumerator MenuCountdown() 
+    {
+        yield return new WaitForSeconds(2.5f);
+        print("Halfway to load");
+        yield return new WaitForSeconds(2.5f);
+        SceneManager.LoadScene(1);
+    }
+}

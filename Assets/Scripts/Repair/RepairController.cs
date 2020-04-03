7b8339c0825924481926b703bf3944347522138c
David Kelly <davek032@outlook.com>
2020-04-03 20:17:41 +0100

    This scene will be finished some point this week

---
+++ Assets/Scripts/Repair/RepairController.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+
+public class RepairController : MonoBehaviour
+{
+    public GameObject hammer;
+    public GameObject nails;
+    public GameObject jack;
+    public GameObject ironBolts;
+
+    bool hasHammer;
+    bool hasNails;
+    bool hasJack;
+    bool hasIronbolts;
+
+    GameObject currentlyLookingAt;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        currentlyLookingAt = collision.gameObject; 
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        currentlyLookingAt = null;
+    }
+}

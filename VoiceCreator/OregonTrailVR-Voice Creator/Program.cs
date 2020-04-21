7ef2f1c2126fd7366cf5b99e03cabd8f50d5a43d
David Kelly <davek032@outlook.com>
2020-04-21 15:21:32 +0100

    Better Speech :D
    Wrote a program to hook into microsoft's AWS compute platform to leverage their much more complex text-to-speech systems so the program can now be used to create somewhat more natural sounding voices.

---
+++ VoiceCreator/OregonTrailVR-Voice Creator/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.CognitiveServices.Speech;
+using Microsoft.CognitiveServices.Speech.Audio;
+using System.IO;
+
+namespace OregonTrailVR
+{
+    class Program
+    {
+        static string path = "Output";
+        static string fileName;
+        static string fileContents;
+        public static async Task SynthesisToAudioFileAsync()
+        {
+            var config = SpeechConfig.FromSubscription("7a21490acf47414aa0d79dfb5f483183", "westeurope");
+
+            var audioFileName = fileName;
+            using (var fileOutput = AudioConfig.FromWavFileOutput(audioFileName))
+            {
+                using (var synthesizer = new SpeechSynthesizer(config, fileOutput))
+                {
+                    var text = fileContents;
+                    var result = await synthesizer.SpeakTextAsync(text);
+
+                    if (result.Reason == ResultReason.SynthesizingAudioCompleted)
+                    {
+                        Console.WriteLine($"Speech synthesized to [{audioFileName}] for text [{text}]");
+                        File.Copy(audioFileName, path + "/" + audioFileName);
+                        Console.WriteLine("Press 'y' to create another file, else press 'n' to quit");
+                        string input = Console.ReadLine();
+                        if (input == "y")
+                        {
+                            fileName = null;
+                            fileContents = null;
+                            createAudioFile();
+                        }
+                    }
+                    else if (result.Reason == ResultReason.Canceled)
+                    {
+                        var cancellation = SpeechSynthesisCancellationDetails.FromResult(result);
+                        Console.WriteLine($"CANCELED: Reason={cancellation.Reason}");
+
+                        if (cancellation.Reason == CancellationReason.Error)
+                        {
+                            Console.WriteLine($"CANCELED: ErrorCode={cancellation.ErrorCode}");
+                            Console.WriteLine($"CANCELED: ErrorDetails=[{cancellation.ErrorDetails}]");
+                            Console.WriteLine($"CANCELED: Did you update the subscription info?");
+                        }
+                    }
+                }
+            }
+        }
+
+        private static void createDirectory()
+        {
+            if (Directory.Exists(path))
+            {
+                Console.WriteLine("Directory already exists, woopsie!");
+            }
+            else
+            {
+                DirectoryInfo di = Directory.CreateDirectory(path);
+            }
+        }
+
+        public static void getInput() 
+        {
+            Console.WriteLine("Enter File Name:");
+            fileName = Console.ReadLine();
+            fileName += ".wav";
+            Console.WriteLine("Enter File Contents:");
+            fileContents = Console.ReadLine();
+        }
+
+        static void Main()
+        {
+            createDirectory();
+
+            createAudioFile();
+        }
+
+        private static void createAudioFile()
+        {
+            getInput();
+            SynthesisToAudioFileAsync().Wait();
+        }
+    }
+}
---
+++ VoiceCreator/README.txt
@@ -0,0 +1,3 @@
+'OregonTrailVR-Voice Creator' was created by David Kelly for his final year AVP module on the Games Technology Course at UWE bristol. It leverages the speech systems of microsoft's AWS cloud computing platform in order to provide a much more natural sounding voice.
+
+PLEASE DO NOT USE THIS FOR YOUR OWN PROJECTS - I DON'T WANT TO GET A SUPRISE BILL
